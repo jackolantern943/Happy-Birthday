69d6e0c886d605b22ed86270c574a84393ac6df3
jackolantern943 <110547876+jackolantern943@users.noreply.github.com>
2023-03-27 17:45:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg'), 
+    function(Img) {
+        block_image_object = Img;
+        block_image_object.scaleToWidth(600);
+        block_image_object.scaleToHeight(400);
+        block_image_object.set()
+        top:0
+        left:0
+        canvas.add('BirthdayImage.jpg') 
+   }
+}
+
+function playSound(){
+    x.play();
+    }
